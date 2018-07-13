7555a75ad4656f7e46a76d0fde2c023d2001b471
jonmoon74 <jon.moon@outlook.com>
2018-07-13 20:29:54 +0100

    initial from tableview built and functioning

---
+++ SnapchatClone/Models/Snaps.swift
@@ -0,0 +1,15 @@
+//
+//  Snaps.swift
+//  SnapchatClone
+//
+//  Created by Jon Moon on 13/07/2018.
+//  Copyright © 2018 Jon Moon. All rights reserved.
+//
+
+import Foundation
+
+class Snap {
+    var imageURL = ""
+    var descrip = ""
+    var from = ""
+}
--- SnapchatClone/View Controllers/SnapsViewController.swift
+++ SnapchatClone/View Controllers/SnapsViewController.swift
@@ -7,15 +7,46 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseAuth
+import FirebaseDatabase
 
-class SnapsViewController: UIViewController {
-
+class SnapsViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    var snaps : [Snap] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        tableView.dataSource = self
+        tableView.delegate = self
+        
+        
+        Database.database().reference().child("users").child(Auth.auth().currentUser!.uid).child("snaps").observe(DataEventType.childAdded, with: { (snapshot) in
+            print(snapshot)
+            let value = snapshot.value as? NSDictionary
+            let snap = Snap()
+            snap.imageURL = value!["imageURL"] as! String
+            snap.from = value!["from"] as! String
+            snap.descrip = value!["description"] as! String
+            
+            self.snaps.append(snap)
+            self.tableView.reloadData()
+        })
     }
-
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return snaps.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        let snap = snaps[indexPath.row]
+        cell.textLabel?.text = snap.from
+        return cell
+    }
     @IBAction func logoutTapped(_ sender: Any) {
         dismiss(animated: true, completion: nil)
     }
