ff99021299849c37765ec366c118f46c704a0ba2
jonmoon74 <jon.moon@outlook.com>
2018-07-19 20:52:28 +0100

    end of lesson commit but still unable to access download url to parse the picture through, will look again later

--- SnapchatClone/View Controllers/PictureViewController.swift
+++ SnapchatClone/View Controllers/PictureViewController.swift
@@ -26,6 +26,7 @@ class PictureViewController: UIViewController, UIImagePickerControllerDelegate, 
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        nextButton.isEnabled = false
         
         // Do any additional setup after loading the view.
         imagePicker.delegate = self
@@ -36,7 +37,7 @@ class PictureViewController: UIViewController, UIImagePickerControllerDelegate, 
         
         imageView.image = image
         imageView.backgroundColor = UIColor.clear
-        
+        nextButton.isEnabled = true
         imagePicker.dismiss(animated: true, completion: nil)
         
     }
--- SnapchatClone/View Controllers/SelectUserViewController.swift
+++ SnapchatClone/View Controllers/SelectUserViewController.swift
@@ -53,7 +53,7 @@ class SelectUserViewController: UIViewController, UITableViewDataSource, UITable
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         let user = users[indexPath.row]
-        let snap = ["from" : user.email, "description" : descrip, "imageURL" : imageURL, "uuid" : uuid]
+        let snap = ["from" : Auth.auth().currentUser!.email, "description" : descrip, "imageURL" : imageURL, "uuid" : uuid]
         Database.database().reference().child("users").child(user.uid).child("snaps").childByAutoId().setValue(snap)
         
         navigationController!.popToRootViewController(animated: true)
